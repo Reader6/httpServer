45b33c8cc93a7af7fc8935d3bcf2ecac732a627d
RownH <799698579@qq.com>
2020-06-25 15:41:44 +0800

    修改Mysql测试用例

--- main/main.cpp
+++ main/main.cpp
@@ -3,51 +3,58 @@
 #include "httpserver/httpServer.h"
 #include "mysqldb/mysql_guard.h"
 #include "log/easylogging++.h"
+#include <mysql/mysql.h>
 using namespace std;
 
 INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char* argv[])
 {
-/*
+
 	{
 		mysql_guard sql_guard;
 		try
 		{
-			MYSQL_RES* result = mysql_store_result(sql_guard());
-			string sql = "select * from user;";
-			mysql_query(sql_guard(), sql.c_str());
-			if (!result)
-				throw string("MySQL not result!");
+			char temp[400];
+			char *tbname = "user";
+			int rt;
+			sprintf(temp,"select * from %s;",tbname);
+			MYSQL_RES *res;
+			MYSQL_ROW row;
+			rt = mysql_real_query(sql_guard(),temp,strlen(temp));
+			if(rt == 0){
+				cout<<"select * from "<<tbname<<": sucess!"<<endl;
+				
+				res = mysql_store_result(sql_guard());
+				while (row = mysql_fetch_row(res)){
+		
+					for(int i = 0; i < mysql_num_fields(res);i++){
+						cout<< row[i]<< " "; 
+					}		
+					cout<<endl;
+				}
+			
+			}else{
+				cout<<"select * from "<<tbname<<": error !: "<<mysql_error(sql_guard())<<endl;
 
-			//获取字段数量
-			int num_fields = mysql_num_fields(result);
-			if (0 == num_fields)
-				throw string("MySQL fields number is 0!");
-
-			//获取字段名
-			MYSQL_FIELD* fields = mysql_fetch_fields(result);
-			if (!fields)
-				throw string("MySQL fields fetch is error!");
-
-			for (int i = 0; i < num_fields; i++)
-			{
-				cout << "field " << i << " name is " << fields[i].name << endl;
 			}
-
-			cout << "MySQL is OK." << endl;
+		
 		}
 		catch (string& error_msg)
 		{
 			cout << error_msg << endl;
+			return 0;
 		}
 		catch (...)
 		{
 			cout << "MySQL operation is error!" << endl;
+			return 0;
 		}
+
 		cout << "MySQL is OK." << endl;
+		return 1;
 	};
-	*/
+	/*
 	//加载日志配置文件
 	el::Configurations conf("my_log.conf");
 	el::Loggers::reconfigureAllLoggers(conf);
@@ -59,6 +66,7 @@ int main(int argc, char* argv[])
 	httpServer* Server = new httpServer(port, ip, 64, 0);
 	Server->event_listen();
 	Server->event_loop();
+	*/
 	return 0;
 }
 
